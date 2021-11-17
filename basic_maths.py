67f7e03fc324c848c18951d395ab4df8bf2c2df2
Simon <sp2053@cam.ac.uk>
2021-11-17 16:46:16 +0100

    Initial commit

---
+++ basic_maths.py
@@ -0,0 +1,32 @@
+"""
+basic math library.
+"""
+
+
+def add(a, b):
+    """
+    add a and b
+    """
+    return a+b
+
+
+def minus(a, b):
+    """
+    subtract b from a
+    """
+    return a-b
+
+
+def multiply(a, b):
+    """
+    multiply a and b
+    """
+    return a*b
+
+
+def divide(a, b):
+    """
+    divide a by b
+    """
+    return a/b
+
