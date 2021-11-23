1fd71ed6921b58ec69df01202b257e143752dc95
Simon <sp2053@cam.ac.uk>
2021-11-23 16:22:32 +0100

    edited test

--- test_conway.py
+++ test_conway.py
@@ -6,12 +6,13 @@ import conway as cw
 import numpy as np
 
 
-def test_conway(t):
+def test_conway():
     """
         Test conway
     """
+    nsteps = 10
     oscillator_init = np.array([[0,0,0,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,0,0,0]])
-    game = cw.conway(array=oscillator_init,t=t)
+    game = cw.conway(array=oscillator_init,t=nsteps)
     oscillator_state1 = game[0::2,:,:]
     oscillator_state2 = game[1::2,:,:]
     if len(oscillator_state1) != len(oscillator_state2):
