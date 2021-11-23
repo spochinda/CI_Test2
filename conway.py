18258a33b5a196cbe7248043c583c86eea05e6db
Simon <sp2053@cam.ac.uk>
2021-11-23 15:29:01 +0100

    Added conway game and test

---
+++ conway.py
@@ -0,0 +1,42 @@
+"""
+conway game of life.
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as scp
+
+def conway(array,t):
+    if array is None:
+        grid_dim = (25,25)
+        grid = np.random.randint(low=0, high=2,size=(grid_dim[0],grid_dim[1]))
+    else:
+        if isinstance(array,list):
+            print('')
+            grid = np.array(array)
+        else:
+            grid = array
+        
+        grid_dim = grid.shape
+        
+    filt = np.ones(shape=(3,3))
+    filt[1,1] = 0
+
+    nsteps = t
+    grid_history = np.empty(shape=(nsteps+1,grid_dim[0],grid_dim[1]))
+    grid_history[0,:,:] = grid
+    
+    for i in range(nsteps):
+        neighbors = scp.convolve2d(grid, filt, mode='same', boundary='symm', fillvalue=0)
+
+        overpop = np.where(neighbors>3)
+        underpop = np.where(neighbors<2)
+        reprod = np.where(neighbors==3)
+
+        grid[overpop] = 0
+        grid[underpop] = 0
+        grid[reprod] = 1
+
+        grid_history[i+1,:,:] = grid
+    return grid_history
---
+++ test_conway.py
@@ -0,0 +1,24 @@
+"""
+test conway.py.
+"""
+
+import conway as cw
+
+
+def test_conway(t):
+    """
+        Test conway
+    """
+    oscillator_init = np.array([[0,0,0,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,0,0,0]])
+    game = cw.conway(array=oscillator_init,t=t)
+    oscillator_state1 = game[0::2,:,:]
+    oscillator_state2 = game[1::2,:,:]
+    if len(oscillator_state1) != len(oscillator_state2):
+        start=1
+    else:
+        start=0
+    print(oscillator_state1.shape,oscillator_state2.shape)
+    comparisson = oscillator_state1[start::,:,:] == oscillator_state2.transpose(0,2,1)
+    assert(comparisson.all())
+
+#test_conway(t=15)
